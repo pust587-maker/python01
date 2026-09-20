dd277ad58b54ab606289d2e6ec1960e1955512f1
pust587-maker <pust587@gmail.com>
2026-09-20 19:45:13 +0300

    we make grade code

--- lesson.py
+++ lesson.py
@@ -1,13 +1,34 @@
 
 
-# def add(name, age = 30):
-#     return(f"Hello, {name}! I am {age} years old.")
+# # def add(name, age = 30):
+# #     return(f"Hello, {name}! I am {age} years old.")
 
-# print(add("Mohamed"))
+# # print(add("Mohamed"))
 
-print("Hello students , welcome to python course")
+# print("Hello students , welcome to python course")
 
-print("Hello students , mohamed we welcoe new language") 
-print("I enjoy learning Python") 
-print("THIS IS MY NIGT FIRST CODE")
- 
+# print("Hello students , mohamed we welcoe new language") 
+# print("I enjoy learning Python") 
+# print("THIS IS MY NIGT FIRST CODE")
+name= "axmed cali madaxay"
+age =60
+print(f" my  full name is { name}, my age is {age}")
+name_pust= "PUST UNIVERSITY"
+print(f"my name is {name}, my age is {age}, my university is  {name_pust}")
+marks = int(input("Enter your Grade: "))
+if(marks >= 200):
+  print("invalid marks")
+elif(marks==90):
+  print("Grade A")
+  
+elif(marks==80):
+  print("Grade B")
+  
+elif(marks==70):
+  print("Grade C")
+  
+elif(marks==60):
+  print("Grade D")
+else:
+  print("Fail")
+  
