d877d9ffa3b0d38db6cb9ec926efb8e351fbfd43
pust587-maker <pust587@gmail.com>
2026-09-20 19:17:53 +0300

    this is code we make practice

--- lesson.py
+++ lesson.py
@@ -1,6 +1,13 @@
 
 
-def add(name, age = 30):
-    return(f"Hello, {name}! I am {age} years old.")
+# def add(name, age = 30):
+#     return(f"Hello, {name}! I am {age} years old.")
 
-print(add("Mohamed"))
+# print(add("Mohamed"))
+
+print("Hello students , welcome to python course")
+
+print("Hello students , mohamed we welcoe new language") 
+print("I enjoy learning Python") 
+print("THIS IS MY NIGT FIRST CODE")
+ 
