a774fdbdf86717e41def4f8c301c91725aec1530
pust587-maker <pust587@gmail.com>
2026-09-20 20:02:37 +0300

    printing customer information

---
+++ customer.py
@@ -0,0 +1,16 @@
+name="mohamed ali geedi madaxay"
+acc_balance=1000
+status = "active"
+email="mohamadaxay@gmail.com"
+bank_name="amalbank"
+print("======customer infrmation======")
+print()
+print(F"MY NAME IS {name}")
+print("==========================")
+print(F"MY acc_balance IS {acc_balance}")
+print("==========================")
+print(F"MY status IS {status}")
+print("==========================")
+print(F"MY email IS {email}")
+print("==========================")
+print(F"MY bank_name  IS {bank_name}")
